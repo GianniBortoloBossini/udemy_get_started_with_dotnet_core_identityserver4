b8d83009789dce6d8d8b5153701175996c9994c1
GianniBortoloBossini <bossini@mailup.com>
2020-03-19 18:44:21 +0100

    How to secure calls with resource owner password auth grant type

--- BankOfDotNet.IdentitySvr/Config.cs
+++ BankOfDotNet.IdentitySvr/Config.cs
@@ -1,10 +1,27 @@
 using System.Collections.Generic;
+using IdentityServer4;
 using IdentityServer4.Models;
+using IdentityServer4.Test;
 
 namespace BankOfDotNet.IdentitySvr
 {
     public class Config
     {
+        public static List<TestUser> GetUsers() {
+            return new List<TestUser> {
+                new TestUser {
+                    SubjectId = "100",
+                    Username = "greg",
+                    Password = "gecko"
+                },
+                new TestUser {
+                    SubjectId = "101",
+                    Username = "gattoboy",
+                    Password = "connor"
+                }
+            };
+        }
+
         public static IEnumerable<ApiResource> GetAllApiResources() {
             return new List<ApiResource> {
                 new ApiResource("bankOfDotNetApi", "Customer Api for BankOfDotNet")
@@ -24,6 +41,18 @@ namespace BankOfDotNet.IdentitySvr
                         new Secret("secret".Sha256())
                     },
                     AllowedScopes={
+                        "bankOfDotNetApi"
+                    }
+                },
+
+                // Resource owner password
+                new Client() {
+                    ClientId = "ro.client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets= {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = {
                         "bankOfDotNetApi"
                     }
                 }
